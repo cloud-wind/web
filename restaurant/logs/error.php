b9e0e34385260e28cdda01c63454ff317ee8e30c
cloud-wind <xn.fishing@gmail.com>
2012-08-25 13:29:58 +0800

    first

---
+++ restaurant/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-08-23 04:12:31 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-08-23	04:12:31	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-08-23	04:12:37	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-08-23	04:12:49	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-08-23	04:13:17	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-08-23	04:13:22	INFO	127.0.0.1	Joomla CANCELED: 	
+2012-08-23	04:13:35	INFO	127.0.0.1	Joomla CANCELED: 	
